78ba53b7b68e867dc82cad549dcf85e61f881b72
Dev (alt) <dev@173-168-100-37.res.bhn.net>
2017-11-06 18:47:54 +0530

    commit 2

--- task1.rb
+++ task1.rb
@@ -2,20 +2,18 @@
 
 
 file_one_name, file_two_name = ARGV
+
+puts "opening file one.............."
+
 file_one  = open(file_one_name)
 file_one_content = file_one.read
-puts "content of file one : \n"
-puts file_one_content
 
 
 #sort words of file one
-
+puts "sorting file one.............."
 
 words = file_one_content.split(' ')
 sorted_words = words.sort
-puts "sorted words are: "
-print sorted_words
-print "\n"
 
 
 #removing words with lenght 3 or less
@@ -37,9 +35,10 @@ content = array_of_words.join(' ')
 #open target file in write mode
 
 
-puts "copying content to target file.........."
+puts "copying sorted content to target file.........."
 
 file_two = open(file_two_name, 'w')
+file_two.truncate(0)
 file_two.write(content)
 
 
